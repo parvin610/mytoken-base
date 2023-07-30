a06a919adb462248ac9ffc0a4d462a6620bc130a
parvin610 <93069048+parvin610@users.noreply.github.com>
2023-07-31 00:14:43 +0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0b43dc49e2ffeffa733d3d12591055478068cbd0";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
